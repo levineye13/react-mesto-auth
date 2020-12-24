86e5316d7ef177febeacd11eaf2cf352713d27fd
Oleg Zharov <levineye@gmail.com>
2020-12-24 11:45:52 +0300

    feat: использовать деструктуризацию

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const PopupWithForm = (props) => {
+const PopupWithForm = ({
+  title,
+  name,
+  buttonText,
+  children,
+  isOpen,
+  onClose,
+  onScreenClickClose,
+  onSubmit,
+  isValid,
+}) => {
   const modalElement = document.getElementById('modal');
 
   return ReactDOM.createPortal(
     <section
-      className={`popup popup_type_${props.name} ${
-        props.isOpen ? 'popup_opened' : ''
-      }`}
-      onClick={props.onScreenClickClose}
+      className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}
+      onClick={onScreenClickClose}
     >
       <div className="popup__wrapper">
         <button
           className="popup__close-button button"
-          onClick={props.onClose}
+          onClick={onClose}
           type="button"
         />
         <form
           action="#"
-          id={props.name}
+          id={name}
           className="popup__form"
-          name={`popup-${props.name}`}
-          onSubmit={props.onSubmit}
+          name={`popup-${name}`}
+          onSubmit={onSubmit}
           noValidate
         >
-          <h2 className="popup__title">{props.title}</h2>
-          {props.children}
+          <h2 className="popup__title">{title}</h2>
+          {children}
           <button
             className={`popup__save-button button ${
-              props.isValid ? '' : 'button_inactive'
+              isValid ? '' : 'button_inactive'
             }`}
             type="submit"
-            disabled={!props.isValid}
+            disabled={!isValid}
           >
-            {props.buttonText}
+            {buttonText}
           </button>
         </form>
       </div>
