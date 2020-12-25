4ec3132be5bbda5458a2f79e9304b903b7736bc9
Oleg Zharov <levineye@gmail.com>
2020-12-25 20:02:37 +0300

    feat: добавить класс для аутентификации пользователя

---
+++ src/utils/auth.js
@@ -0,0 +1,72 @@
+class Auth {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponceStatus = async (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error('Ошибка');
+  };
+
+  register = async ({ password, email }) => {
+    try {
+      const res = await fetch(`${this._baseUrl}/signup`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          password,
+          email,
+        }),
+      });
+      const data = await this._checkResponceStatus(res);
+      return data;
+    } catch (err) {
+      console.error(`${err.name} - ${err.message}`);
+    }
+  };
+
+  authorize = async ({ password, email }) => {
+    try {
+      const res = await fetch(`${this._baseUrl}/signin`, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          password,
+          email,
+        }),
+      });
+      const data = await this._checkResponceStatus(res);
+      return data;
+    } catch (err) {
+      console.error(`${err.name} - ${err.message}`);
+    }
+  };
+
+  checkToken = async ({ token }) => {
+    try {
+      const res = await fetch(`${this._baseUrl}/users/me`, {
+        method: 'GET',
+        headers: {
+          ...this._headers,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await this._checkResponceStatus(res);
+      return data;
+    } catch (err) {
+      console.error(`${err.name} - ${err.message}`);
+    }
+  };
+}
+
+const auth = new Auth({
+  baseUrl: 'https://auth.nomoreparties.co',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default auth;
