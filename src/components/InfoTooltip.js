ec12e565f81f162bed0ab199dfd0ecf79bee090e
Oleg Zharov <levineye@gmail.com>
2020-12-25 12:47:06 +0300

    feat: сверстать попап c всплывающей подсказкой

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const InfoTooltip = ({ onClose, isSuccessful = false }) => {
+  const modalElement = document.getElementById('modal');
+
+  return ReactDOM.createPortal(
+    <section className="popup popup_type_info-tooltip">
+      <div className="popup__wrapper popup__wrapper_type_info-tooltip">
+        <button className={'popup__close-button button'} onClick={onClose} />
+        <figure className="popup__img-container popup__img-container_type_info-tooltip">
+          <div
+            className={`popup__circle ${
+              isSuccessful
+                ? 'popup__circle_type_accept'
+                : 'popup__circle_type_error'
+            }`}
+          ></div>
+          <figcaption className="popup__title-img popup__title-img_type_info-tooltip">
+            {isSuccessful
+              ? 'Вы успешно зарегистрировались!'
+              : 'Что-то пошло не так! Попробуйте ещё раз.'}
+          </figcaption>
+        </figure>
+      </div>
+    </section>,
+    modalElement
+  );
+};
+
+export default InfoTooltip;
