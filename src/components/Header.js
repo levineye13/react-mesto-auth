977475cd2b067f26c6bc27189c171a48f3874a16
Oleg Zharov <levineye@gmail.com>
2020-12-27 15:58:12 +0300

    feat: добавить константы пути

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { signIn, signUp } from '../utils/constants.js';
 import headerLogo from './../images/header__logo.svg';
 
 const Header = function ({ loggedIn }) {
@@ -11,22 +12,22 @@ const Header = function ({ loggedIn }) {
     const path = location.pathname;
 
     const setData = (path) => {
-      if (path === '/sign-in') {
-        setUrl('/sign-up');
+      if (path === signIn) {
+        setUrl(signUp);
         setText('Регистрация');
-      } else if (path === '/sign-up') {
-        setUrl('/sign-in');
+      } else if (path === signUp) {
+        setUrl(signIn);
         setText('Войти');
       }
     };
 
     setData(path);
-  }, [location]);
+  }, [location.pathname]);
 
   return (
     <header className="header">
       <img src={headerLogo} alt="Логотип Место" className="header__logo" />
-      <Link to={loggedIn ? '/sign-in' : url} className="header__link">
+      <Link to={loggedIn ? signIn : url} className="header__link">
         {loggedIn ? 'Выйти' : text}
       </Link>
     </header>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-
+export const signIn = '/sign-in';
+export const signUp = '/sign-up';
